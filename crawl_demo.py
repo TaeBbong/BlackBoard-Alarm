3f2ca98372659b057f4eb31a32b0dce3100aecd3
TaeBbong <mok05289@korea.ac.kr>
2018-10-15 00:49:09 +0900

    Init Project

---
+++ crawl_demo.py
@@ -0,0 +1,68 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import smtplib
+from email.mime.text import MIMEText
+from config import *
+
+driver = webdriver.Chrome(get_chrome_driver())
+driver.implicitly_wait(3)
+
+driver.get(get_target_url())
+
+driver.find_element_by_name('id').send_keys(get_blackboard_id())
+driver.find_element_by_name('pw').send_keys(get_blackboard_pw())
+driver.find_element_by_xpath('//*[@id="entry-login"]').click()
+
+driver.get('https://kulms.korea.ac.kr/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_2_1')
+
+subject_list = [('130456_1', '2200239_1'), ('130455_1', 0), ('130904_1', 0), ('129758_1', '2197447_1'), ('128802_1', '2193627_1'), ('128798_1', 0), ('130469_1', '2200291_1')]
+subject_url_1 = 'https://kulms.korea.ac.kr/webapps/blackboard/execute/announcement?method=search&context=course_entry&course_id=_'
+subject_url_2 = '&handle=announcements_entry&mode=view'
+subject_url_3 = 'https://kulms.korea.ac.kr/webapps/blackboard/content/listContent.jsp?course_id=_'
+subject_url_4 = '&content_id=_'
+subject_url_5 = '&mode=reset'
+
+smtp = smtplib.SMTP('smtp.gmail.com', 587)
+smtp.ehlo()
+smtp.starttls()
+smtp.login(get_gmail_id(), get_gmail_pw())
+
+for subject in subject_list:
+    # Get Announcements
+    subject_announcement_url = subject_url_1 + subject[0] + subject_url_2
+    driver.get(subject_announcement_url)
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    announcements = soup.select('li.clearfix')[10:]
+    announcements.reverse()
+
+    # Check DB & Send Mail of Announcements
+    for ann in announcements:
+        print(ann.attrs['id'])
+        print('---------------')
+        break
+        # msg = MIMEText(ann.text)
+        # msg['Subject'] = 'Announcement for ' + subject[0]
+        # msg['To'] = get_user_mail()
+        # smtp.sendmail(get_gmail_id(), get_user_mail, msg.as_string())
+
+    # Get Assignments if exists
+    if subject[1] != 0:
+        subject_assignment_url = subject_url_3 + subject[0] + subject_url_4 + subject[1] + subject_url_5
+        driver.get(subject_assignment_url)
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        assignments = soup.select('ul.contentList > li')
+
+        # Check DB & Send Mail of Assignments
+        for ass in assignments:
+            print(ass.attrs['id'])
+            break
+            # msg = MIMEText(ass.text)
+            # msg['Subject'] = 'Assignment for ' + subject[0]
+            # msg['To'] = get_user_mail()
+            # smtp.sendmail(get_gmail_id(), get_user_mail(), msg.as_string())
+
+    break
+
+smtp.quit()
---
+++ readme.md
@@ -0,0 +1,23 @@
+# BlackBoard - Alarm
+* BlackBoard Alarm Project for my own purpose
+* Based on Korea University BlackBoard
+
+# Stacks
+* Python 3.6
+* Selenium
+* Beautifulsoup of BS4
+* Gmail System with SMTP
+* MYSQL DB Server on my local desktop
+
+# How does it work
+* Crawls user's subject list.
+* Goes to each page.
+* Gets Announcement list & Assignment list.
+* Compares with MYSQL DB.
+* If new stuff exists, inserts into DB Table then send mail to user (Alarm!).
+* Works on 9:00 & 17:00 for EVERYDAY!
+
+# License
+* FREE to use, but have to keep comments that TaeBbong wrote this code originally.
+* or send mail to me for commercial purpose. (mok05289@naver.com)
+* PULL REQUEST PLZ! THX:)
